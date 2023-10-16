4693d73f27e1a7d0678daa13c4577998fff245ea
Gustavo Cerda <102986954+GustavCerda@users.noreply.github.com>
2023-10-16 14:50:44 -0300

    Update Ciclos HolaMundo.cs
    Empezamos definiendo como imprimir hola mundo de distintas formas

--- Ciclos HolaMundo.cs
+++ Ciclos HolaMundo.cs
@@ -1,1 +1,20 @@
+//Imprime el texto entre comillas en consola
+Console.Writeline("Hola Mundo");
+//Con variable
+var Hola = "Hola Mundo";
+Console.Writeline(Hola);
+// Asignando un nuevo valor
+Hola = "Chao Mundo";
+Console.Writeline(Hola);
+//Como Funcion
+//Crea Funcion
+static String HolaMundo(string hola ){
+  
+  return hola;
+  
+}
+
+
+
+
 
